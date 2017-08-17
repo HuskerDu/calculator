3233bf781e8ef19d60ef40419fcd7b45019e2216
HuskerDu <adithyajr11@gmail.com>
2017-08-17 19:10:09 +0530

    Evaluation functions moved to separate file

--- Calculator/calc.py
+++ Calculator/calc.py
@@ -5,71 +5,21 @@
 
 from tkinter import *
 from tkinter import ttk
-from operator import add, sub, mul, truediv
+import rpolish
 
 equation = ''
 
-####################################################
-#Converting from infix notation to postfix notation#
-####################################################
 
-def infix_to_post(s):
-    try:
-        stack, b = [], []
-        a = s.split()
-        for i in a:
-            if i not in '/*-+':
-                b.append(i)
-            else:
-                if not stack:
-                    stack.append(i)
-                else:
-                    if stack[-1] in '+-' and i in '*/':
-                        stack.append(i)
-                    else:
-                        while stack:
-                            b.append(stack.pop())
-                        stack.append(i)
-        while stack:
-            b.append(stack.pop())
-        return ' '.join(b)
-    except:
-        return 0
-
-        
-        
-##########################
-#Solving postfix notation#
-##########################
-
-
-def postfix_solve(s):
-    try:
-        op = {'+': add, '-': sub, '*': mul, '/': truediv}
-        stack = []
-        c = s.split()
-        for i in c:
-            if i in op:
-                a, b = stack.pop(), stack.pop()
-                stack.append(op[i](float(b), float(a)))
-            elif i:
-                stack.append(i)
-        return stack.pop()
-    except:
-        return 0
-
-        
 ###################################
 #Getting the equation to be solved#
 ###################################
-
 
 def get_eqn(s):
     global equation
     try:
         if s=='=':
-            reverse_polish = infix_to_post(equation)
-            ok = postfix_solve(reverse_polish)
+            reverse_polish = rpolish.infix_to_post(equation)
+            ok = rpolish.postfix_solve(reverse_polish)
             if not ok:
                 raise ValueError
             equation=''
