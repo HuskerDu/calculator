b07e097599278ff083f1e67b6b3d44f404924e5a
HuskerDu <adithyajr11@gmail.com>
2017-08-18 21:44:50 +0530

    Update calc.py

--- Calculator/calc.py
+++ Calculator/calc.py
@@ -1,16 +1,19 @@
-##############################
-# Created by HuskerDu (Pixie)#
-##############################
+###############################
+# Created by HuskerDu (Pixie) #
+###############################
 
 from tkinter import *
 from tkinter import ttk
 import gui
+
+#Creating basic framework for the GUI
 
 root = Tk()
 root.title('Calculator')  # Setting GUI Title
 
 window = ttk.Frame(root, padding='10 10 10 10')
 window.grid(row=0, column=0, sticky=(N, W, E, S))
-gui.stu(ttk, window, root)
+
+gui.graphics(ttk, window, root) #calling graphics function from gui file for the entire Calculator framework
 
 root.mainloop()
