0d2ea40df986d35d4e290d0a7b7b4f7bdf35ced9
HuskerDu <adithyajr11@gmail.com>
2017-08-18 19:37:19 +0530

    Added support for negative numbers

--- Calculator/calc.py
+++ Calculator/calc.py
@@ -23,18 +23,22 @@ def get_eqn(s):
                 del(equation[-1])
                 equation = ''.join(equation)
                 eqn.set(equation)        
-        elif s=='=':
+        elif s == '=':
             reverse_polish = rpolish.infix_to_post(equation)
             ok = rpolish.postfix_solve(reverse_polish)
-            if not ok:
+            if ok is 'No':
                 raise ValueError
             equation=''
             eqn.set(ok)
         elif s in '+-/*':
-            equation+=" "+s+" "
+            if len(equation) == 0 or len(equation) > 2 and equation[-2] in '+-*/':
+                if s in '+-':
+                    equation += s
+            else:
+                equation += " {} ".format(s)
             eqn.set(equation)
         else:
-            equation+= s
+            equation += s
             eqn.set(equation)
     except:
         eqn.set('Error')
