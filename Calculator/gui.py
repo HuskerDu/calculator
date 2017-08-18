b39168561af9e75b3b3e3dd61148708a8294a820
HuskerDu <adithyajr11@gmail.com>
2017-08-18 21:47:08 +0530

    Update gui.py

--- Calculator/gui.py
+++ Calculator/gui.py
@@ -1,14 +1,16 @@
 import display
 from tkinter import *
 
-def stu(ttk, window, root):
+def graphics(ttk, window, root):
 
-    eqn = StringVar()
+    eqn = StringVar() # Variable for interacting with the GUI
     
     stuff = ttk.Frame(window)
     stuff.grid(row=0, column=0, columnspan=3, sticky=E)
     ttk.Label(stuff, textvariable=eqn).grid(row=0, column=1)
-
+    
+    # Creating the buttons on the interface
+    
     ttk.Button(window, text='7', command=lambda: display.get_eqn('7', eqn)).grid(row=1, column=0, sticky=(N, W, E, S))
     ttk.Button(window, text='8', command=lambda: display.get_eqn('8', eqn)).grid(row=1, column=1, sticky=(N, W, E, S))
     ttk.Button(window, text='9', command=lambda: display.get_eqn('9', eqn)).grid(row=1, column=2, sticky=(N, W, E, S))
@@ -27,6 +29,7 @@ def stu(ttk, window, root):
     ttk.Button(window, text='+', command=lambda: display.get_eqn('+', eqn)).grid(row=4, column=3, sticky=(N, W, E, S))
     ttk.Button(window, text='Del', command=lambda: display.get_eqn('Del', eqn)).grid(row=1, column=4, sticky=(N, W, E, S))
     
+    # Resizing window and making it more responsive
     
     stuff.rowconfigure(0, weight=1)
     stuff.columnconfigure(0, weight=1)
@@ -37,7 +40,8 @@ def stu(ttk, window, root):
         window.columnconfigure(i, weight=1, pad='80')
         root.columnconfigure(i, weight=1, pad='80')
 
-   
+    # Taking input from keyboard
+    
     root.bind('7', lambda x: display.get_eqn('7', eqn))
     root.bind('8', lambda x: display.get_eqn('8', eqn))
     root.bind('9', lambda x: display.get_eqn('9', eqn))
